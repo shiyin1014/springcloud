1dad9f52f413910aa865ff09c9b3caf31a715a3c
杜世银 <981179142@qq.com>
2020-04-20 01:02:39 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/dsy/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.dsy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2020/4/20
+ * Package com.dsy.springcloud
+ *
+ * @author dsy
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
